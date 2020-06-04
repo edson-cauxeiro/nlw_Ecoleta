835e9d0f612184f6a397dbef1a2d8abbc3154fd4
edsonabelha <edson.cauxeiro87@gmail.com>
2020-06-04 21:25:07 +0100

    Configurando o Servidor,Routas e Pasta Publica

--- src/server.js
+++ src/server.js
@@ -1,6 +1,24 @@
 const express = require("express")
-
 const server = express()
+
+//configurar pasta publica
+server.use(express.static("public"))
+
+//Configurar caminhos da minha aplicação
+//pagina inicial
+//req-> Requisição
+//res-> Respostas do Servidor
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+
+server.get("/search-results", (req, res) => {
+    res.sendFile(__dirname + "/views/search-results.html")
+})
 
 //ligar o servidor
 server.listen(3000)
