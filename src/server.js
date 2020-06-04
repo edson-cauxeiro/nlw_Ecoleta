08cda19c94b919b205d034518915a06a3ceb66f6
edsonabelha <edson.cauxeiro87@gmail.com>
2020-06-04 22:21:16 +0100

    Instalação e Configuração do Tamplate Engine Nunjucks

--- src/server.js
+++ src/server.js
@@ -4,20 +4,27 @@ const server = express()
 //configurar pasta publica
 server.use(express.static("public"))
 
+//utilizando template engine
+const nunjuncks = require("nunjucks")
+nunjuncks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 //Configurar caminhos da minha aplicação
 //pagina inicial
 //req-> Requisição
 //res-> Respostas do Servidor
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+   return res.render("index.html")
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
 })
 
 server.get("/search-results", (req, res) => {
-    res.sendFile(__dirname + "/views/search-results.html")
+   return res.render("search-results.html")
 })
 
 //ligar o servidor
